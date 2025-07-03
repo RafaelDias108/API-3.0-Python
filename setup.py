577e67c07b1fbcfcdb3b5fb6d5086cfa074d6930
Rafael Dias <rafael.dias@gigage.dev>
2025-07-03 16:30:32 -0300

    chore: adicionado arquivo setup de configurações

--- setup.py
+++ setup.py
@@ -1,63 +1,21 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
 
-import os
-import sys
-
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup
-
-settings = dict()
-
-
-# Publish Helper.
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist upload')
-    sys.exit()
-
-settings.update(
-    name='cieloApi3',
-    version='0.1.5',
-    description='SDK API-3.0 Python Cielo',
-    author='Thiago Malaquias',
-    author_email='thiago.malaca@gmail.com',
-    url='https://github.com/thiago-Malaca/API-3.0-Python',
-    keywords='api3.0 cielo python sdk ecommerce',
+setup(
+    name="cieloApi3",
+    version="0.2.0",
+    author="Rafael Dias",
+    author_email="rafaelsd70@gmail.com",
+    description="SDK não oficial para integração com a API 3.0 de pagamento da Cielo em Python.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/RafaelDias108/API-3.0-Python",
     packages=find_packages(),
-    install_requires=['requests'],
-    license='MIT',
-    classifiers=(
-        # 'Development Status :: 5 - Production/Stable',
+    install_requires=['requests', 'future'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
         'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        # 'Programming Language :: Python :: 2.5',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-    )
+    ],
+    python_requires='>=3.7',
 )
-
-
-setup(**settings)
-
-
-
-
-# # -*- coding: utf-8 -*-
-# from setuptools import setup
-
-# setup(
-#     name='cieloApi3',
-#     version='0.1.1',
-#     url='',
-#     license='MIT License',
-#     author='Thiago Malaquias',
-#     author_email='',
-#     keywords='api3.0 cielo python sdk ecommerce',
-#     description=u'',
-#     packages=['cielo.api30'],
-#     install_requires=['requests'],
-# )
